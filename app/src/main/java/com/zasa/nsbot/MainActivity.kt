2d335804e664658a517d6c7d8539a1ed7b8474e4
zansangeeth <zansangeeth17@gmail.com>
2022-09-29 11:03:10 +0530

    updated UI, implemented session time out

--- app/src/main/java/com/zasa/nsbot/MainActivity.kt
+++ app/src/main/java/com/zasa/nsbot/MainActivity.kt
@@ -1,8 +1,16 @@
 package com.zasa.nsbot
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.MotionEvent
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
 import com.zasa.nsbot.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,11 +18,20 @@ class MainActivity : AppCompatActivity() {
     //binding
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    // Declaring handler, runnable and time in milli seconds
+    private lateinit var mHandler: Handler
+    private lateinit var mRunnable: Runnable
+    private var mTime: Long = 60000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         replaceFragment(Home())
+
+        firebaseAuth = FirebaseAuth.getInstance()
 
         binding.bottomNavigationView.setOnItemSelectedListener {
             when (it.itemId) {
@@ -26,6 +43,44 @@ class MainActivity : AppCompatActivity() {
             }
             true
         }
+
+        // Initializing the handler and the runnable
+        mHandler = Handler(Looper.getMainLooper())
+        mRunnable = Runnable {
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Session Time Out")
+            builder.setMessage("user is inactive for one minute")
+            builder.setIcon(R.drawable.ic_baseline_add_alert_24)
+            builder.setPositiveButton("Ok"){dialogInterface, which ->
+                firebaseAuth.signOut()
+                startActivity(Intent(this, LoginActivity::class.java))
+                finish()
+            }
+            builder.show()
+        }
+
+        // Start the handler
+        startHandler()
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        // Removes the handler callbacks (if any)
+        stopHandler()
+
+        // Runs the handler (for the specified time)
+        // If any touch or motion is detected before
+        // the specified time, this override function is again called
+        startHandler()
+
+        return super.onTouchEvent(event)
+    }
+
+    private fun stopHandler() {
+        mHandler.removeCallbacks(mRunnable)
+    }
+
+    private fun startHandler() {
+        mHandler.postDelayed(mRunnable, mTime)
     }
 
     // replace fragment layout
