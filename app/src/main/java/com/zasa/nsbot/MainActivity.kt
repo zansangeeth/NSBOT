fabaffd2c203f6a2f07b3f69d91eee3367793e98
zansangeeth <zansangeeth17@gmail.com>
2022-09-29 12:08:43 +0530

    deleted unused imports

--- app/src/main/java/com/zasa/nsbot/MainActivity.kt
+++ app/src/main/java/com/zasa/nsbot/MainActivity.kt
@@ -1,13 +1,11 @@
 package com.zasa.nsbot
 
 import android.content.Intent
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.MotionEvent
-import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import com.google.firebase.auth.FirebaseAuth
